e1d5408105e5755ba0228fb915172fdbc20c813a
ristaputrii <ayaputre@gmail.com>
2025-10-27 07:49:19 +0700

    Add files via upload

---
+++ JS 1/profile_script.js
@@ -0,0 +1,10 @@
+let myName = "Rista Putri";
+const myAge = 17;
+
+console.log(Nama= " + myName");
+console.log(Umur= " + myAge");
+
+document.getElementById("nama-saya").innerHTML = "Halo, saya Rista Wiranti Cahya Putri"
+document.getElementById("info-diri").innerHTML = "Saya berusia 17 tahun dan saya suka belajar pemrograman."
+
+alert("Anda telah mengklik tombol!");
